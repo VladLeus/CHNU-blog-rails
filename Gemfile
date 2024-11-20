6c9b6747f538ba07255a19fdf9e46c2401e88d3c
VladLeus <vladyslavleus@gmail.com>
2024-11-21 00:13:10 +0200

    feat: added admin panel

--- Gemfile
+++ Gemfile
@@ -18,9 +18,11 @@ gem "stimulus-rails"
 gem "jbuilder"
 # Use Redis adapter to run Action Cable in production
 gem "redis", ">= 4.0.1"
+gem "activeadmin"
 gem "devise"
 gem "bootstrap", "~> 5.3.0"
 gem "sass-rails"
+gem "pundit"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,12 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true, length: { in: 10..100 }
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[body created_at id post_id updated_at user_id]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[post user]
+  end
 end
